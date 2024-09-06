da12c13283fe893e60dae2cac7d32d87120298c0
TowfMinh <vinxen2019@gmail.com>
2024-09-06 10:20:53 +0700

    update login service api

--- src/service/jwt.service.ts
+++ src/service/jwt.service.ts
@@ -4,9 +4,9 @@ const accessTokenKey = process.env.ACCESS_TOKEN_KEY || ''
 const refreshTokenKey = process.env.REFRESH_TOKEN_KEY || ''
 
 export const generateAccessToken = (userID: string) => {
-  return jwt.sign(userID, accessTokenKey, { expiresIn: '15m' })
+  return jwt.sign({ userID }, accessTokenKey, { expiresIn: '15m' })
 }
 
 export const generateRefreshToken = (userID: string) => {
-  return jwt.sign(userID, refreshTokenKey, { expiresIn: '7d' })
+  return jwt.sign({ userID }, refreshTokenKey, { expiresIn: '7d' })
 }
