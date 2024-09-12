0cd9df99b34b3741e9c33b5bec10b7fc81e83565
TowfMinh <vinxen2019@gmail.com>
2024-09-12 09:30:57 +0700

    update product schema

---
+++ drizzle/0005_regular_sphinx.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "brands" DROP COLUMN IF EXISTS "brand_id";--> statement-breakpoint
+ALTER TABLE "categories" DROP COLUMN IF EXISTS "category_id";
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,11 +1,92 @@
-// import { Product } from '@/types/product'
+// import { getDbClient } from '@/database/connection'
+// import { tblBrands, tblCategories, tblProducts } from '@/models/product.schema'
+// import { Specification } from '@/types/product'
+// import { eq } from 'drizzle-orm'
 
-// export const addProduct = async (
+// export const insertProduct = async (
 //   name: string,
 //   image: string,
 //   price: number,
-//   discount?: boolean,
-//   percent?: number,
-//   salePrice?: number,
-//   color: string
-// ): Promise<Product | string> => {}
+//   color: string,
+//   category: string,
+//   brand: string,
+//   specifications: Specification[],
+//   discount: boolean,
+//   percent: number,
+//   salePrice: number
+// ): Promise<string> => {
+//   const db = getDbClient()
+
+//   let categoryID
+//   let brandID
+
+//   //check existed category
+//   const existedCategory = await db
+//     .select()
+//     .from(tblCategories)
+//     .where(eq(tblCategories.categoryName, category))
+//     .limit(1)
+
+//   if (existedCategory.length === 0) {
+//     const newCategory = await db
+//       .insert(tblCategories)
+//       .values({ categoryName: category, totalProducts: 1 })
+//       .returning()
+
+//     categoryID = newCategory[0].id
+//   } else {
+//     categoryID = existedCategory[0].id
+
+//     await db
+//       .update(tblCategories)
+//       .set({
+//         totalProducts: (existedCategory[0].totalProducts as number) + 1
+//       })
+//       .where(eq(tblCategories.categoryName, category))
+//   }
+
+//   //check existed Brand
+//   const existedBrand = await db
+//     .select()
+//     .from(tblBrands)
+//     .where(eq(tblBrands.brandName, brand))
+//     .limit(1)
+//   if (existedBrand.length === 0) {
+//     const newBrand = await db
+//       .insert(tblBrands)
+//       .values({ brandName: brand, totalProducts: 1 })
+//       .returning()
+//     brandID = newBrand[0].id
+//   } else {
+//     brandID = existedBrand[0].id
+
+//     await db
+//       .update(tblBrands)
+//       .set({
+//         totalProducts: (existedBrand[0].totalProducts as number) + 1
+//       })
+//       .where(eq(tblBrands.brandName, brand))
+//   }
+
+//   const productSchemaValue = {
+//     name,
+//     image,
+//     price,
+//     color,
+//     categoryID,
+//     brandID,
+//     specifications,
+//     discount,
+//     percent,
+//     salePrice
+//   }
+
+//   const productResult = await db
+//     .insert(tblProducts)
+//     .values(productSchemaValue)
+//     .returning()
+
+//   const productID = productResult[0].id
+
+//   return 'add successs'
+// }
