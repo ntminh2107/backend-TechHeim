afd410cd3e6244f676c300292255ee9dfac44fd5
TowfMinh <vinxen2019@gmail.com>
2024-09-17 14:29:22 +0700

    create order + cart schema

---
+++ src/types/cart.ts
@@ -0,0 +1,33 @@
+import { Address } from './user'
+
+export type Cart = {
+  id: string
+  userID: string
+  status: string
+  cartItems: CartItems[]
+}
+
+export type CartItems = {
+  name: string
+  image: string
+  quantity: number
+  price: string
+}
+
+export type OrderItems = {
+  name: string
+  image: string
+  quantity: number
+  price: string
+}
+
+export type Order = {
+  id: string
+  userID: string
+  address: Address
+  status: string
+  orderItems: OrderItems[]
+  total: number
+  createdAt: Date
+  updatedAt: Date
+}
